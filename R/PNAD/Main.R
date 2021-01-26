bf49bc94777675194c7318e2aad9f5f77dc43a49
BordiniAmaral <felipe.bordini.amaral@gmail.com>
2021-01-26 17:23:05 -0300

    Updated README with PNAD R file Updated paths on PNAD R file

--- R/PNAD/Main.R
+++ R/PNAD/Main.R
@@ -2,7 +2,7 @@
 # This file is dedicated to building the income transition matrices for
 # Brasil, based upon PNADc data.
 #
-# 
+#
 # File by: Felipe Bordini do Amaral
 # Institution: FGV - EESP
 # Project: Master Dissertation
@@ -61,48 +61,48 @@ INSS_discount <- function(public, formal, contributes, value, schedule){
 # Defining function for processing PNADc quarterly data
 
 pnadc_income_periods <- function(periods,tax_table,export_path){
-  
+
   # Checking order of periods
-  
+
   periods <- periods %>% arrange(ano,tri)
-  
+
   for(t in 1:length(periods$tri)){
-    
+
     cat(glue("------------------------------ \n Starting year {periods$ano[t]}, quarter {periods$tri[t]} \n \n"))
-    
+
     # Selecting tax schedule for given period
     IR_temp <- tax_table %>%
       filter(ano == periods$ano[t],
              tri == periods$tri[t] | tri == "all",
              type == "IR")
-    
+
     INSS_temp <- tax_table %>%
       filter(ano == periods$ano[t],
              tri == periods$tri[t] | tri == "all",
              type == "INSS")
-    
+
     cat("Fetching PNADc observation... \n")
-    pnadc_df <- get_pnadc(year = periods$ano[t], 
-                          quarter = periods$tri[t], 
-                          design = FALSE, 
+    pnadc_df <- get_pnadc(year = periods$ano[t],
+                          quarter = periods$tri[t],
+                          design = FALSE,
                           labels = FALSE)
-    
+
     current_defla <- pnadc_df %>%
       select(UF, Efetivo) %>%
       distinct()
-    
+
     price_deflator <- ref_prices %>%
       left_join(current_defla, by = "UF") %>%
       mutate(defla = Efetivo / EfetivoRef) %>%
       select(UF, defla)
-    
+
     cat("Working on PNADC observation... \n")
     current_df <- pnadc_df %>%
       mutate(hh_id = str_c(str_pad(UPA,9,pad = "0"), str_pad(V1008,2,pad = "0"), str_pad(V1014,9,pad = "0"))) %>%
       select(Ano, Trimestre, UF, hh_id, V1016,
              VD2003, V2005, V2009,
-             V4028, V4029, V4032, V403412, V403422, 
-             V4047, V4048, V4049, V405112, V405122, 
+             V4028, V4029, V4032, V403412, V403422,
+             V4047, V4048, V4049, V405112, V405122,
              V4057, V405912, V405922) %>%
       left_join(price_deflator, by = "UF") %>%
       mutate_all(~replace(., is.na(.), 0)) %>%
@@ -125,9 +125,9 @@ pnadc_income_periods <- function(periods,tax_table,export_path){
                 V1016 = max(V1016)) %>%
       mutate(hh_net_defla_annualized_pc = hh_net_defla_annualized / hh_members,
              t = t)
-    
+
     cat("Saving current observation... \n")
-    
+
     setwd(export_path)
     fwrite(current_df, file = glue("PNADC_clean_{periods$ano[t]}_{periods$tri[t]}.csv"), sep = ";")
   }
@@ -135,7 +135,7 @@ pnadc_income_periods <- function(periods,tax_table,export_path){
 
 # Using jan/2018 prices to match POF 2017-2018 prices
 # Getting deflators for 1Q2018:
-price_path <- "C:/Backup Acer/EESP-Mestrado/Dissertação/PNADC/deflator_PNADC_2020_trimestral_010203.xls"
+price_path <- "D:/Google Drive/Working Cloud/EESP-Mestrado/Dissertação/PNADC/deflator_PNADC_2020_trimestral_010203.xls"
 ref_prices <- read_xls(price_path) %>%
   filter(Ano == 2018,
          trim == "01-02-03") %>%
@@ -144,7 +144,7 @@ ref_prices <- read_xls(price_path) %>%
 rm(price_path)
 
 # Getting tax and social security contribution schedules:
-tax_path <- "C:/Backup Acer/EESP-Mestrado/Dissertação/PNADC/TributosPrevidencia.xlsx"
+tax_path <- "D:/Google Drive/Working Cloud/EESP-Mestrado/Dissertação/PNADC/TributosPrevidencia.xlsx"
 tax_table <- read_xlsx(tax_path) %>%
   replace_na(list(upper = Inf, deduction = 0))
 rm(tax_path)
@@ -153,7 +153,7 @@ periods <- data.frame(ano = rep(2012:2018, each = 4),
                       tri = rep(1:4, (2018-2012)+1))
 
 # Running PNADc observations
-export_path <- "C:/Backup Acer/EESP-Mestrado/Dissertação/PNADC/R Project/Clean household income"
+export_path <- "D:/Google Drive/Working Cloud/EESP-Mestrado/Dissertação/PNADC/R Project/Clean household income"
 pnadc_income_periods(periods,tax_table,export_path)
 
 #-----------------------------------------------------------------------
@@ -163,7 +163,7 @@ pnadc_income_periods(periods,tax_table,export_path)
 # Imports all files from selected folder
 # Make sure only PNADC_clean csv files are there
 
-import_path <- "C:/Backup Acer/EESP-Mestrado/Dissertação/PNADC/R Project/Clean household income"
+import_path <- "D:/Google Drive/Working Cloud/EESP-Mestrado/Dissertação/PNADC/R Project/Clean household income"
 import_files <- list.files(path = import_path)
 
 pnadc_hh <- map(glue("{import_path}/{import_files}"), fread, sep = ";", dec = ".")
@@ -179,11 +179,11 @@ setwd(export_path)
 quantile_frontiers <- pnadc_hh %>%
   filter(hh_age_head >= 25, hh_age_head <= 66, V1016 %in% c(1,5)) %>%
   group_by(hh_id) %>%
-  mutate(both = (n()==2), 
+  mutate(both = (n()==2),
          same_ref = (max(hh_age_head)-min(hh_age_head)) <= 1) %>%
   ungroup() %>%
   filter(both == TRUE,
-         same_ref == TRUE) %>% 
+         same_ref == TRUE) %>%
   group_by(hh_age_head) %>%
   summarise(q98 = quantile(hh_net_defla_annualized_pc, probs = 0.98, type = 8),
             q96 = quantile(hh_net_defla_annualized_pc, probs = 0.96, type = 8),
@@ -207,11 +207,11 @@ quantile_frontiers <- pnadc_hh %>%
 quantile_values <- pnadc_hh %>%
   filter(hh_age_head >= 25, hh_age_head <= 66, V1016 %in% c(1,5)) %>%
   group_by(hh_id) %>%
-  mutate(both = (n()==2), 
+  mutate(both = (n()==2),
          same_ref = (max(hh_age_head)-min(hh_age_head)) <= 1) %>%
   ungroup() %>%
   filter(both == TRUE,
-         same_ref == TRUE) %>% 
+         same_ref == TRUE) %>%
   group_by(hh_age_head) %>%
   summarise(v98 = quantile(hh_net_defla_annualized_pc, probs = 0.99, type = 8),
             v96 = quantile(hh_net_defla_annualized_pc, probs = 0.97, type = 8),
@@ -245,7 +245,7 @@ quantile_frontiers_current <- pnadc_hh %>%
   ungroup() %>%
   filter(both == TRUE,
          V1016 == 1) %>%
-  filter(hh_age_head >= 25, hh_age_head <= 65) %>% 
+  filter(hh_age_head >= 25, hh_age_head <= 65) %>%
   group_by(hh_age_head) %>%
   summarise(q98 = quantile(hh_net_defla_annualized_pc, probs = 0.98, type = 8),
             q96 = quantile(hh_net_defla_annualized_pc, probs = 0.96, type = 8),
@@ -276,8 +276,8 @@ quantile_frontiers_next <- pnadc_hh %>%
          V1016 == 5) %>%
   mutate(hh_age_head = age_1) %>%
   select(-age_1) %>%
-  filter(hh_age_head >= 25, hh_age_head <= 65) %>% 
-  group_by(hh_age_head) %>% 
+  filter(hh_age_head >= 25, hh_age_head <= 65) %>%
+  group_by(hh_age_head) %>%
   summarise(q98 = quantile(hh_net_defla_annualized_pc, probs = 0.98, type = 8),
             q96 = quantile(hh_net_defla_annualized_pc, probs = 0.96, type = 8),
             q94 = quantile(hh_net_defla_annualized_pc, probs = 0.94, type = 8),
@@ -301,45 +301,45 @@ quantile_frontiers_next <- pnadc_hh %>%
 #                    Creating (t, t+4) pairing
 #-----------------------------------------------------------------------
 
-export_path <- "C:/Backup Acer/EESP-Mestrado/Dissertação/PNADC/R Project/Transitions by age"
+export_path <- "D:/Google Drive/Working Cloud/EESP-Mestrado/Dissertação/PNADC/R Project/Transitions by age"
 setwd(export_path)
 
 compile_pairwise_transition <- function(pnadc_hh, periods){
-  
+
   # Starting final dataframe
-  
+
   complete_df <- data.frame(hh_id = character(),
                             hh_members = integer(),
                             hh_age_head = integer(),
                             y_current = double(),
                             y_next = double())
-  
+
   cat("-------------------------------------\n")
   for(i in 1:periods){
-    
+
     if(i+4>periods){break}
-    
+
     cat(glue("  Running period {i} out of {periods-4} \n \n"))
     # Does not run at pairwise start-up
 
     current_df <- pnadc_hh %>%
       filter(t == i, V1016 == 1) %>%
       rename(y_current = hh_net_defla_annualized_pc) %>%
-      select(hh_id, hh_age_head, hh_members, y_current) 
-    
+      select(hh_id, hh_age_head, hh_members, y_current)
+
     next_df <- pnadc_hh %>%
       filter(t == i+4, V1016 == 5) %>%
       rename(y_next = hh_net_defla_annualized_pc) %>%
       select(hh_id, y_next)
-    
+
     joined_df <- current_df %>%
       left_join(next_df, by = "hh_id") %>%
       filter(!is.na(y_next))
-    
+
     # Adds to complete data-frame
-    
+
     complete_df <- bind_rows(complete_df, joined_df)
-    
+
   }
   cat("             Finish \n")
   cat("-------------------------------------")
@@ -354,34 +354,34 @@ fwrite(complete_transitions, file = "complete_transitions.csv")
 #-----------------------------------------------------------------------
 
 ages <- seq(from = 25, to = 65, by = 1)
-export_path <- "C:/Backup Acer/EESP-Mestrado/Dissertação/PNADC/R Project/Transitions by age"
+export_path <- "D:/Google Drive/Working Cloud/EESP-Mestrado/Dissertação/PNADC/R Project/Transitions by age"
 
 build_transitions <- function(age,
                               quantile_frontiers,
                               quantile_values,
                               complete_transitions,
                               export_path){
-  
+
   cat("----------------------\n")
   cat(glue("Starting for age {age} \n"))
-  
+
   setwd(export_path)
-  
+
   current_frontier <- quantile_frontiers_current %>%
     filter(hh_age_head == age) %>%
     arrange(value)
-  
+
   next_frontier <- quantile_frontiers_next %>%
     filter(hh_age_head == age) %>%
     arrange(value)
-  
+
   current_transition <- complete_transitions %>%
     select(-c("hh_id")) %>%
     filter(hh_age_head == age) %>%
     rowwise() %>%
     mutate(current_q = sum(current_frontier$value < y_current),
            next_q = sum(next_frontier$value < y_next))
-  
+
   transition_matrix <- current_transition %>%
     count(current_q, next_q) %>%
     group_by(current_q) %>%
@@ -390,30 +390,30 @@ build_transitions <- function(age,
     select(-n) %>%
     pivot_wider(names_from = next_q, names_prefix = "q", values_from = freq, values_fill = 0) %>%
     arrange(current_q)
-  
+
   # Building a proper matrix for this transition
   row.names(transition_matrix) <- glue("q{transition_matrix$current_q}")
   quantiles <- seq(from = 0, to = 17, by = 1)
   matrix_1 <- as.matrix(transition_matrix[,glue("q{quantiles}")])
-  
+
   fwrite(matrix_1, file = glue("{age}yo_matrix1.csv"))
-  
+
   # Density plot of current transition
-  
+
   quantile_points <- c(seq(0.02,0.08,0.02),
                        seq(0.1,0.9,0.1),
                        seq(0.92,1,0.02))
-  
+
   current_transition <- current_transition %>%
     mutate(current_quantile = quantile_points[as.integer(current_q)+1],
            next_quantile = quantile_points[as.integer(next_q)+1])
-  
+
   quantile_points <- data.frame(current_quantile = quantile_points,
                                 x_vline = quantile_points)
-  
+
   current_transition$current_quantile <- as.factor(current_transition$current_quantile)
   quantile_points$current_quantile <- as.factor(quantile_points$current_quantile)
-  
+
   ggplot(current_transition)+
     geom_density(aes(next_quantile, colour = current_quantile, fill = current_quantile), adjust = 4, alpha = 0.1)+
     facet_wrap(~current_quantile) +
@@ -428,7 +428,7 @@ build_transitions <- function(age,
            width = 10,
            height = 8,
            dpi = 300)
-  
+
   return(current_transition)
 }
 
@@ -503,7 +503,7 @@ ggplot(transitions_with_quantile)+
        x = "Next quantile",
        y = "Density",
        colour = "Current quantile",
-       fill = "Current quantile") + 
+       fill = "Current quantile") +
   theme(legend.position = "none") +
   scale_x_continuous(breaks = seq(0,1,0.5)) +
   ggsave(filename = glue("average_distribution.png"),
@@ -520,7 +520,7 @@ ggplot(transitions_with_quantile)+
        x = "Next quantile",
        y = "Density",
        colour = "Current quantile",
-       fill = "Current quantile") + 
+       fill = "Current quantile") +
   theme(legend.position = "none") +
   scale_x_continuous(breaks = seq(0,1,0.5)) +
   ggsave(filename = glue("average_distribution_step.png"),
@@ -530,8 +530,8 @@ ggplot(transitions_with_quantile)+
 
 # Compiling average # of household members by age and quantile
 
-members_by_quantile <- transitions_with_quantile %>% 
-  group_by(hh_age_head, current_q) %>% 
+members_by_quantile <- transitions_with_quantile %>%
+  group_by(hh_age_head, current_q) %>%
   summarise(mean_num = mean(hh_members))
 
 setwd(export_path)
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Author: Felipe Bordini do Amaral
 
 This repository holds the files used for my Masters' thesis
 
-1. R/POF/tidying_data.R file Tidying POF data
+## 1. R/POF/tidying_data.R file Tidying POF data
 
   Requires .RDS files from "Pesquisa de Orçamento Familiares 2018"
   Requires 'Cadastro de Produto.xls' file, which categorizes all consumption as temptation or non-temptation
@@ -16,7 +16,7 @@ This repository holds the files used for my Masters' thesis
   Returns 'consumption_total.csv' containing item-by-item annualized consumption by household id
   Returns 'morador_count.csv' containing number of members by household id
 
-2. .R file Tidying PNAD data and creating transitions and state-spaces
+## 2. R/POF/Stoney-Geary.R file organizing and comparing calibrations A, B and C
 
   Requires 'consumption_total.csv' and 'morador_count.csv' files from step 1.
   
@@ -25,7 +25,20 @@ This repository holds the files used for my Masters' thesis
   Runs regression for case A and C, requires input from GMM for case B
   Plots comparison graphs for the three calibrations
 
-3. .R file conducting parametric estimates
+## 3. R/POF/Graphics.R file generating additional plots
+
+  Requires 'consumption_total.csv' and 'morador_count.csv' files from step 1.
 
 
-4. .py file with heterogeneous-agent model, calibration and graphs
+## 4. R/PNAD/Main.R file Tidying PNAD data and creating transitions and state-spaces
+
+  Imports and saves PNAD files (do only once, it takes some time)
+  Requires 'deflator_PNADC_2020_trimestral_010203.xls' file from IBGE
+  Requires 'TributosPrevidencia.xlsx' with tax and social security schedules
+  Deducts individual tax and social security schedule
+  
+  Returns 'quantile_frontiers.csv' and 'quantile_values.csv' defining the measures and values for the disposable income per capita state-space
+  Returns 'complete_transitions.csv' with all PNADC (t,t+4) transitions for the period
+  Returns matrices and graphs for transition (by age and aggregate)
+
+## 5. .py file with heterogeneous-agent model, calibration and graphs
