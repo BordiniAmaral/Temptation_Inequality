160a1d00c67968fbae34fde624edcca2c452e2bf
BordiniAmaral <felipe.bordini.amaral@gmail.com>
2021-01-28 16:59:11 -0300

    Partial revision of Calibration code (this will be the main and only calibration file after all is done. CalibrationB will be dropped and will exist only as history)

--- Python/Calibration.py
+++ Python/Calibration.py
@@ -23,18 +23,15 @@ def import_POF_data(file_path):
     return pof_df
 
 @njit
-def run_simulations_by_x0(grid_sigx, grid_sigy, x0, data_x, data_y, gridm, xi, consumpt, cons_bin, y_data_sel_sum, W):
+def run_simulations_by_x0(grid_gam1, grid_gam2, x0, grid_x0, data_x, data_y, gridm, consumpt, cons_bin, y_data_sel_sum, W):
     
     print("------------------------ \n   Current x0:",x0,"  \n------------------------\n")
-    sqe = np.zeros(shape = (len(grid_sigx), len(grid_sigy)))
+    sqe = np.zeros(shape = (len(grid_gam1), len(grid_gam2)))
     counter = 0
-    sigy_start = np.int32(0)
-    for sigx in range(len(grid_sigx)):
-        for sigy in range(sigy_start, len(grid_sigy)):
-            
-            if grid_sigy[sigy] > grid_sigx[sigx]:
-                sqe[sigx,sigy:] = np.Inf #arbitrary high number
-                break
+    gam2_start = np.int32(0)
+    
+    for gam1 in range(len(grid_gam1)):
+        for gam2 in range(gam2_start, len(grid_gam2)):
             
             y_sim = np.zeros(shape = (len(data_x)))
             x_sim = np.zeros(shape = (len(data_x)))
@@ -43,28 +40,12 @@ def run_simulations_by_x0(grid_sigx, grid_sigy, x0, data_x, data_y, gridm, xi, c
                 if consumpt[x] < (x0 + 2):
                     x_sim[x] = consumpt[x]
                 else:
-                    x_sim[x] = Newton(f, df, (x0 + 0.0001), 1e-6,int(1e6), consumpt[x], xi, grid_sigx[sigx], grid_sigy[sigy], x0)
+                    x_sim[x] = Newton(f, df, (x0 + 0.0001), 1e-6,int(1e6), consumpt[x], gam1, gam2, x0)
                     y_sim[x] = (data_x[x] + data_y[x]) - x_sim[x]
-                
-                # In case you need y(x): y_sim[x] = np.exp((grid_sigy[sigy]/grid_sigx[sigx])*np.log(x_sim[x]-x0) - grid_sigy[sigy]*np.log((grid_sigx[sigx] - 1)/(grid_sigy[sigy]-1)*(grid_sigy[sigy]/grid_sigx[sigx])))
             
-            sqe[sigx,sigy] = calculate_sqe(y_sim, data_x, data_y, x0, gridm, cons_bin, y_data_sel_sum, W)[0]
-            
-            # Not the first sigy evaluated
-            if sigy > sigy_start:
-                # Checking if sigy passed minimum
-                if sqe[sigx,sigy] > sqe[sigx,sigy-1]:
-                    sqe[sigx,:sigy_start] = np.Inf
-                    sqe[sigx,(sigy+1):] = np.Inf
-                    sigy_start = np.max(np.array([sigy - 2, np.int64(0)]))
-                    break
-                # Checking if sigy did not reach minimum
-                elif sigy == len(grid_sigy):
-                    sqe[sigx,:sigy_start] = np.Inf
-                    sigy_start = np.max(np.array([sigy - 1, np.int64(0)]))
-
+            sqe[gam1,gam2] = calculate_sqe(y_sim, data_x, data_y, gridm, cons_bin, y_data_sel_sum, W)[0]
                     
-        progress = np.floor((sigx/len(grid_sigx))*100)
+        progress = np.floor((gam1/len(grid_gam1))*100)
         if (progress / 20) > counter :
             counter = counter + 1
             print(progress, " % of x0 =",x0,"concluded")
@@ -73,7 +54,7 @@ def run_simulations_by_x0(grid_sigx, grid_sigy, x0, data_x, data_y, gridm, xi, c
 
 # Calculating squared error of moments
 @njit
-def calculate_sqe(y_sim, data_x, data_y, x0, gridm, cons_bin, y_data_sel_sum, W):
+def calculate_sqe(y_sim, data_x, data_y, gridm, cons_bin, y_data_sel_sum, W):
     
     m_errors = np.zeros(shape = len(gridm))
     y_sim_sel_sum = np.zeros(shape = len(gridm))
@@ -88,27 +69,27 @@ def calculate_sqe(y_sim, data_x, data_y, x0, gridm, cons_bin, y_data_sel_sum, W)
         
         
 @njit
-def f(x, c, xi, sigma_x, sigma_y, x0):
-    return x + (x-x0)**(sigma_y/sigma_x)*(((sigma_x-1)/sigma_x)*(sigma_y/(xi*(sigma_y-1))))**(-sigma_y) - c
+def f(x, c, gam1, gam2, x0):
+    return x + np.exp(gam1)*(x-x0)**gam2 - c
 
 @njit
-def df(x, c, xi, sigma_x, sigma_y, x0):
-    return 1 + sigma_y/sigma_x * (x-x0)**((sigma_y-sigma_x)/sigma_x)*(((sigma_x-1)/sigma_x)*(sigma_y/(xi*(sigma_y-1))))**(-sigma_y)
+def df(x, c, gam1, gam2, x0):
+    return 1 + np.exp(gam1)*gam2*(x-x0)**(gam2-1)
 
 @njit
-def Newton(f, df, x_init, e, maxit, c, xi, sigma_x, sigma_y, x0):
+def Newton(f, df, x_init, e, maxit, c, gam1, gam2, x0):
     xn = x_init
     for n in range(0,maxit):
-        fxn = f(xn, c, xi, sigma_x, sigma_y, x0)
+        fxn = f(xn, c, gam1, gam2, x0)
         if abs(fxn)<e:
             return xn
-        dfxn = df(xn, c, xi, sigma_x, sigma_y, x0)
+        dfxn = df(xn, c, gam1, gam2, x0)
         if dfxn == 0:
             print("Reached zero derivative in iteration",n,"please review initial guess")
             return None
         xn = xn - fxn/dfxn
     print("Maximum iterations (",n,") reached. \n Current x:",xn,"\n Current f(x):",fxn)
-    print("\n C evaluated: ", c,"\n sx evaluated: ", sigma_x,"\n sy evaluated: ", sigma_y)
+    print("\n C evaluated: ", c,"\n gam1 evaluated: ", gam1,"\n gam2 evaluated: ", gam2)
     return None
 
 def create_cons_moment_grid(which):
@@ -158,19 +139,19 @@ def create_cons_moment_grid(which):
     return gridm
 
 @njit
-def run_x0_simulations(grid_sigx, grid_sigy, grid_x0, data_x, data_y, gridm, xi, W):
+def run_x0_simulations(grid_gam1, grid_gam2, grid_x0, data_x, data_y, gridq, W):
     
     # Starting timer
     # start_time = time.time()
     print("Running initial calculations... \n")
     
-    sqe = np.zeros(shape = (len(grid_x0), len(grid_sigx), len(grid_sigy)))
-    consumpt, cons_bin, y_data_sel_sum = initial_computations(data_x,data_y,gridm)
+    sqe = np.zeros(shape = (len(grid_x0), len(grid_gam1), len(grid_gam2)))
+    consumpt, cons_bin, y_data_sel_sum, gridm = initial_computations(data_x, data_y, gridq, grid_x0)
     
     print("Proceeding to simulations... \n")
     for x0 in range(len(grid_x0)):
         
-        sqe[x0,:,:] = run_simulations_by_x0(grid_sigx, grid_sigy, grid_x0[x0], data_x, data_y, gridm, xi, consumpt, cons_bin, y_data_sel_sum, W)
+        sqe[x0,:,:] = run_simulations_by_x0(grid_gam1, grid_gam2, grid_x0[x0], grid_x0, data_x, data_y, gridm, consumpt, cons_bin, y_data_sel_sum, W)
         
         #hour = round(((time.time() - start_time)/60)//60)
         #minute = round((time.time() - start_time)//60 - hour*60)
@@ -180,14 +161,14 @@ def run_x0_simulations(grid_sigx, grid_sigy, grid_x0, data_x, data_y, gridm, xi,
     
     return sqe
 
-@njit
-def run_two_steps(grid_sigx, grid_sigy, grid_x0, data_x, data_y, gridm, xi):
+@njit # THIS FUNCTION HAS NOT BEEN REVISED!!
+def run_two_steps(grid_sigx, grid_sigy, grid_x0, data_x, data_y, gridq, grid_xi):
     
-    W = np.identity(len(gridm))
+    W = np.identity(len(gridq))
     
     print("Running first simulation...\n")
     
-    sqe_I = run_x0_simulations(grid_sigx, grid_sigy, grid_x0, data_x, data_y, gridm, xi, W)
+    sqe_I = run_x0_simulations(grid_sigx, grid_sigy, grid_x0, data_x, data_y, gridq, grid_xi, W)
     
     print("Recalculating weight matrix...\n")
     
@@ -195,22 +176,24 @@ def run_two_steps(grid_sigx, grid_sigy, grid_x0, data_x, data_y, gridm, xi):
     sol_x0_1 = grid_x0[sol[0][0]]
     sol_sigx_1 = grid_sigx[sol[0][1]]
     sol_sigy_1 = grid_sigy[sol[0][2]]
-    errors_sol = simulate_single(sol_sigx_1, sol_sigy_1, sol_x0_1, data_x, data_y, gridm, xi, W)[1]
+    sol_xi_1 = grid_xi[sol[0][3]]
+    errors_sol = simulate_single(sol_sigx_1, sol_sigy_1, sol_x0_1, data_x, data_y, gridq, sol_xi_1, W, grid_x0)[1]
     Omega, W_new = recalculate_weights(errors_sol)
     
-    print(" First stage concluded!\n\n Parameters found: \n x0:",sol_x0_1,"\n sigx:",sol_sigx_1,"\n sigy:",sol_sigy_1)
+    print(" First stage concluded!\n\n Parameters found: \n x0:",sol_x0_1,"\n sigx:",sol_sigx_1,"\n sigy:",sol_sigy_1,"\n xi:",sol_xi_1)
     
     print("Running second simulation...\n")
     
-    sqe_W = run_x0_simulations(grid_sigx, grid_sigy, grid_x0, data_x, data_y, gridm, xi, W_new)
+    sqe_W = run_x0_simulations(grid_sigx, grid_sigy, grid_x0, data_x, data_y, gridq, grid_xi, W_new)
     
     sol = np.argwhere(sqe_W == np.min(sqe_W))
     sol_x0_2 = grid_x0[sol[0][0]]
     sol_sigx_2 = grid_sigx[sol[0][1]]
     sol_sigy_2 = grid_sigy[sol[0][2]]
+    sol_xi_2 = grid_xi[sol[0][3]]
     
-    print(" (First Stage Recap)\n\n Parameters found: \n x0:",sol_x0_1,"\n sigx:",sol_sigx_1,"\n sigy:",sol_sigy_1,"\n---------------------------")
-    print(" Second stage concluded!\n\n Parameters found: \n x0:",sol_x0_2,"\n sigx:",sol_sigx_2,"\n sigy:",sol_sigy_2)
+    print(" (First Stage Recap)\n\n Parameters found: \n x0:",sol_x0_1,"\n sigx:",sol_sigx_1,"\n sigy:",sol_sigy_1,"\n xi:",sol_xi_1,"\n---------------------------")
+    print(" Second stage concluded!\n\n Parameters found: \n x0:",sol_x0_2,"\n sigx:",sol_sigx_2,"\n sigy:",sol_sigy_2,"\n xi:",sol_xi_2)
 
     return sqe_I, sqe_W, W_new, Omega
 
@@ -220,17 +203,17 @@ def select_data(pof_df):
     
     return data_x, data_y
 
-@njit
+@njit # THIS FUNCTION HAS NOT BEEN REVISED!!
 def recalculate_weights(m_errors):
     Omega = (1/len(m_errors))* (m_errors.reshape((len(m_errors),1)) @ m_errors.reshape((1,len(m_errors))))
     W_new = np.linalg.pinv(Omega)
     
     return Omega, W_new
 
-@njit
-def simulate_single(sigx, sigy, x0, data_x, data_y, gridm, xi, W):
+@njit # THIS FUNCTION HAS NOT BEEN REVISED!!
+def simulate_single(sigx, sigy, x0, data_x, data_y, gridq, xi, W, grid_x0):
     
-    consumpt, cons_bin, y_data_sel_sum = initial_computations(data_x,data_y,gridm)
+    consumpt, cons_bin, y_data_sel_sum, gridm = initial_computations(data_x, data_y, gridq, grid_x0)
     
     y_sim = np.zeros(shape = (len(data_x)))
     x_sim = np.zeros(shape = (len(data_x)))
@@ -242,25 +225,30 @@ def simulate_single(sigx, sigy, x0, data_x, data_y, gridm, xi, W):
             x_sim[x] = Newton(f, df, (x0 + 0.0001), 1e-6,int(1e6), consumpt[x], xi, sigx, sigy, x0)
             y_sim[x] = (data_x[x] + data_y[x]) - x_sim[x]
     
-    sqe, m_errors = calculate_sqe(y_sim, data_x, data_y, x0, gridm, cons_bin, y_data_sel_sum, W)
+    x0_index = np.argwhere(grid_x0 == x0)[0][0]
+    sqe, m_errors = calculate_sqe(y_sim, data_x, data_y, x0_index, gridm, cons_bin, y_data_sel_sum, W)
     
     return sqe, m_errors
     
 @njit
-def initial_computations(data_x, data_y, gridm):
+def initial_computations(data_x, data_y, gridq, grid_x0):
     
     consumpt = data_x + data_y
     
     # Creating array to hold which consumption bin each consumption belongs to
-    cons_bin = np.zeros(shape = len(consumpt))
+    cons_bin = np.zeros(shape = (len(consumpt)))
     
-    # Filling cons_bin accordingly
-    for c in range(len(consumpt)): 
-        cons_bin[c] = np.sum(gridm < consumpt[c]) - 1.0
-        
     # Creating array with the sum of temptation consumption in each bin
-    y_data_sel_sum = np.zeros(shape = (len(gridm)))
+    y_data_sel_sum = np.zeros(shape = (len(gridq)))
+    
+    # Creating grid of moments
+    gridm = np.quantile(consumpt,gridq)
+    
+    for c in range(len(consumpt)): 
+            cons_bin[c] = np.int64(np.sum(gridm < consumpt[c]) - 1)
+            
     for b in range(len(gridm)):
         y_data_sel_sum[b] = np.sum(data_y[cons_bin == b])
+    
         
-    return consumpt, cons_bin, y_data_sel_sum
+    return consumpt, cons_bin, y_data_sel_sum, gridm
